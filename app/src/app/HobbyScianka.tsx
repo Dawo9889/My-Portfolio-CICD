cdb7a3c2dfc4755af93f4e28fd556f9385af0da9
Dawo9889 <dawoltis9889@gmail.com>
2025-06-11 17:46:38 +0200

    update to work in container

--- app/src/app/HobbyScianka.tsx
+++ app/src/app/HobbyScianka.tsx
@@ -14,21 +14,22 @@ export function HobbyScianka() {
           color="transparent"
           shadow={false}
           className="py-8 lg:flex-row"
+          placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
         >
-          <CardBody className="w-full lg:gap-10 h-full lg:!flex justify-between">
+          <CardBody className="w-full lg:gap-10 h-full lg:!flex justify-between" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
             {/* Sekcja tekstowa */}
             <div className="w-full mb-10 lg:mb-0">
               <Typography
                 variant="h3"
                 color="blue-gray"
-                className="mb-4 font-bold lg:max-w-xs"
+                className="mb-4 font-bold lg:max-w-xs" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
               >
                 3. Wspinaczka ściankowa
               </Typography>
-              <Typography className="mb-3 w-full lg:w-8/12 font-normal !text-gray-500">
+              <Typography className="mb-3 w-full lg:w-8/12 font-normal !text-gray-500" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                 Wspinaczka ściankowa to dla mnie świetny sposób na poprawę siły, techniki i koncentracji. Zacząłem od boulderowni, ale z czasem przerzuciłem się też na wspinanie z liną. Wymaga skupienia, planowania i dobrej współpracy z partnerem asekuracyjnym — co przekłada się też na inne obszary życia.
               </Typography>
-              <Typography variant="h6" color="blue-gray" className="mb-0.5">
+              <Typography variant="h6" color="blue-gray" className="mb-0.5" placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}>
                 Pare zdjęć
               </Typography>
               <div className="flex items-center gap-4">
@@ -39,6 +40,7 @@ export function HobbyScianka() {
                   size="xl"
                   className={`cursor-pointer ${active === 1 ? "opacity-100" : "opacity-50"}`}
                   onClick={() => setActive(1)}
+                  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
                 />
                 <div className="w-[1px] h-[36px] bg-blue-gray-100" />
                 <Avatar
@@ -48,6 +50,7 @@ export function HobbyScianka() {
                   size="xl"
                   className={`cursor-pointer ${active === 2 ? "opacity-100" : "opacity-50"}`}
                   onClick={() => setActive(2)}
+                  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
                 />
                 <div className="w-[1px] h-[36px] bg-blue-gray-100" />
                 <Avatar
@@ -57,6 +60,7 @@ export function HobbyScianka() {
                   size="xl"
                   className={`cursor-pointer ${active === 3 ? "opacity-100" : "opacity-50"}`}
                   onClick={() => setActive(3)}
+                  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
                 />
                 <div className="w-[1px] h-[36px] bg-blue-gray-100" />
                 <Avatar
@@ -66,6 +70,7 @@ export function HobbyScianka() {
                   size="xl"
                   className={`cursor-pointer ${active === 4 ? "opacity-100" : "opacity-50"}`}
                   onClick={() => setActive(4)}
+                  placeholder={undefined} onPointerEnterCapture={undefined} onPointerLeaveCapture={undefined}
                 />
               </div>
             </div>
